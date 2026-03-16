0e607f480682d76cf0b61ebb5e82470dd2ce627a
Xander <87785817+XanderYZZ@users.noreply.github.com>
2026-03-16 12:51:33 -0700

    Initial commit.

--- src/main.py
+++ src/main.py
@@ -1,6 +1,12 @@
+import os
+log_file_name = os.getenv("LOG_FILE_NAME", "log_file.txt")
+
 def main():
-    print("Starting it")
-    
+    with open(log_file_name, 'r') as file:
+        content = file.read()
+        
+        for line in content: 
+            print(line)
 
 if __name__ == "__main__":
     main()
