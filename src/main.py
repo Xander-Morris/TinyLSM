a131fec3b1ca13d7a7bdd2609cb725cd805c8280
Xander <87785817+XanderYZZ@users.noreply.github.com>
2026-03-16 13:00:58 -0700

    Initial commit.

--- src/main.py
+++ src/main.py
@@ -1,10 +1,27 @@
-import os
-log_file_name = os.getenv("LOG_FILE_NAME", "log_file.txt")
+import config
+from classes import kv_store 
 
 def main():
-    with open(log_file_name, 'r') as file:
+    store = kv_store.KVStore()
+
+    with open(config.LOG_FILE_NAME, 'r') as file:
         for line in file: 
-            print(line.strip())
+            line = line.strip() 
+            sp = line.split(" ")
+            operation = sp[0]
+            
+            if operation == "SET":
+                if len(sp) < 3:
+                    print("3 arguments are required for SET!")
+                else:
+                    store.set(sp[1], sp[2])
+            elif operation == "DELETE":
+                if len(sp) < 2:
+                    print("2 arguments required for delete")
+                else:
+                    store.delete(sp[1])
+            else:
+                print(f"Invalid operation: {operation}!")
 
 if __name__ == "__main__":
     main()
