79dc6ea22c941205909943deb1410e23f6ab26a3
Xander <87785817+XanderYZZ@users.noreply.github.com>
2026-03-16 14:58:43 -0700

    Made changes to KV.

--- src/main.py
+++ src/main.py
@@ -1,29 +1,54 @@
 import config
 from classes import kv_store 
 
+def process_line(store, replay=False):
+    line = line.strip() 
+    sp = line.split(" ")
+    operation = sp[0]
+    
+    if operation == "SET":
+        if len(sp) < 3:
+            print("3 arguments are required for SET!")
+        else:
+            if replay:
+                store._set(sp[1], sp[2], True)
+            else:
+                store.set(sp[1], sp[2])
+    elif operation == "DELETE":
+        if len(sp) < 2:
+            print("2 arguments required for delete")
+        else:
+            if replay:
+                store._delete(sp[1], True)
+            else:
+                store.delete(sp[1])
+    elif operation == "GET":
+        if len(sp) < 2:
+            print("2 arguments required for delete")
+        else:
+            return store.get(sp[1], True)
+    else:
+        print(f"Invalid operation: {operation}!")
+
 def main():
     store = kv_store.KVStore(config.LOG_FILE_NAME, config.MAX_ENTRIES, config.MAX_SSTABLES)
 
     try:
         with open(config.LOG_FILE_NAME, 'r') as file:
             for line in file: 
-                line = line.strip() 
-                sp = line.split(" ")
-                operation = sp[0]
-                
-                if operation == "SET":
-                    if len(sp) < 3:
-                        print("3 arguments are required for SET!")
-                    else:
-                        store._set(sp[1], sp[2], True)
-                elif operation == "DELETE":
-                    if len(sp) < 2:
-                        print("2 arguments required for delete")
-                    else:
-                        store._delete(sp[1], True)
-                else:
-                    print(f"Invalid operation: {operation}!")
-            file.close()
+                process_line(line, True)
+        
+        while True:
+            command = input("Enter a command: ")
+
+            if "EXIT" in command:
+                break
+
+            result = process_line(command)
+
+            if result is not None:
+                print(result)
+
     except:
         print("No file exists!")
 
