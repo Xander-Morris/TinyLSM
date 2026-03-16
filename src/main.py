b800b498893489faa8f944e522177bca7f769ebe
Xander <87785817+XanderYZZ@users.noreply.github.com>
2026-03-16 12:53:03 -0700

    Initial commit.

--- src/main.py
+++ src/main.py
@@ -3,10 +3,8 @@ log_file_name = os.getenv("LOG_FILE_NAME", "log_file.txt")
 
 def main():
     with open(log_file_name, 'r') as file:
-        content = file.read()
-        
-        for line in content: 
-            print(line)
+        for line in file: 
+            print(line.strip())
 
 if __name__ == "__main__":
     main()
