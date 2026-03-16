67af9b55e214aaa3263b0e033ed54b2ecbb7c4e5
Xander <87785817+XanderYZZ@users.noreply.github.com>
2026-03-16 15:00:46 -0700

    Made changes to KV.

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 import config
 from classes import kv_store 
 
-def process_line(store, replay=False):
+def process_line(store, line, replay=False):
     line = line.strip() 
     sp = line.split(" ")
     operation = sp[0]
@@ -16,7 +16,7 @@ def process_line(store, replay=False):
                 store.set(sp[1], sp[2])
     elif operation == "DELETE":
         if len(sp) < 2:
-            print("2 arguments required for delete")
+            print("2 arguments are required for DELETE!")
         else:
             if replay:
                 store._delete(sp[1], True)
@@ -24,9 +24,9 @@ def process_line(store, replay=False):
                 store.delete(sp[1])
     elif operation == "GET":
         if len(sp) < 2:
-            print("2 arguments required for delete")
+            print("2 arguments are required for GET!")
         else:
-            return store.get(sp[1], True)
+            return store.get(sp[1])
     else:
         print(f"Invalid operation: {operation}!")
 
@@ -36,21 +36,23 @@ def main():
     try:
         with open(config.LOG_FILE_NAME, 'r') as file:
             for line in file: 
-                process_line(line, True)
-        
+                process_line(store, line, True)
+    except FileNotFoundError:
+        print("No file exists!")
+
+    try: 
         while True:
             command = input("Enter a command: ")
 
             if "EXIT" in command:
                 break
 
-            result = process_line(command)
+            result = process_line(store, command)
 
             if result is not None:
                 print(result)
+    except KeyboardInterrupt:
 
-    except:
-        print("No file exists!")
 
 if __name__ == "__main__":
     main()
