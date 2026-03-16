b0df4c77f1fde349568e23e78f5221edffce4901
Xander <87785817+XanderYZZ@users.noreply.github.com>
2026-03-16 16:04:06 -0700

    Added a basic bloom filter that I must add upon.

---
+++ src/classes/bloom_filter.py
@@ -0,0 +1,36 @@
+import config 
+
+class BloomFilter: 
+    # Static Methods
+    @staticmethod
+    def deserialize(str):
+        filter = BloomFilter(len(str))
+        index = 0
+
+        for char in str: 
+            i = int(char) 
+            filter.bits[index] = i 
+            index += 1
+        
+        return filter 
+
+    def __init__(self, size):
+        self.bits = [0] * size 
+    
+    # Public Methods 
+    def add(self, key):
+        for i in range(config.HASH_FUNCTIONS):
+            index = hash(key + str(i)) % len(self.bits)
+            self.bits[index] = 1
+
+    def contains(self, key):
+        for i in range(config.HASH_FUNCTIONS):
+            index = hash(key + str(i)) % len(self.bits)
+            
+            if self.bits[index] != 1:
+                return False 
+        
+        return True
+    
+    def serialize(self):
+        return "".join(self.bits)
