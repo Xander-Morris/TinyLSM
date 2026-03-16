c24997ad197e9b3ec83a18a30010926daddd0560
Xander <87785817+XanderYZZ@users.noreply.github.com>
2026-03-16 13:08:49 -0700

    Made changes to KV.

--- src/classes/kv_store.py
+++ src/classes/kv_store.py
@@ -1,12 +1,25 @@
 class KVStore:
-    def __init__(self):
+    def __init__(self, log_file_name):
         self._store = {}
+        self.log_file_name = log_file_name 
 
     def set(self, key: str, value: str):
-        self._store[key] = value
+        try:
+            with open(self.log_file_name, 'w') as file:
+                file.write(f"SET {key} {value}")
+                file.close()
+                self._store[key] = value
+        except:
+            print("No file exists!")
 
     def get(self, key: str):
         return self._store.get(key)
 
     def delete(self, key: str):
-        self._store[key] = None
+        try:
+            with open(self.log_file_name, 'w') as file:
+                file.write(f"DELETE {key}")
+                file.close()
+                self._store[key] = None
+        except:
+            print("No file exists!")
--- src/main.py
+++ src/main.py
@@ -4,24 +4,28 @@ from classes import kv_store
 def main():
     store = kv_store.KVStore()
 
-    with open(config.LOG_FILE_NAME, 'r') as file:
-        for line in file: 
-            line = line.strip() 
-            sp = line.split(" ")
-            operation = sp[0]
-            
-            if operation == "SET":
-                if len(sp) < 3:
-                    print("3 arguments are required for SET!")
+    try:
+        with open(config.LOG_FILE_NAME, 'r') as file:
+            for line in file: 
+                line = line.strip() 
+                sp = line.split(" ")
+                operation = sp[0]
+                
+                if operation == "SET":
+                    if len(sp) < 3:
+                        print("3 arguments are required for SET!")
+                    else:
+                        store.set(sp[1], sp[2])
+                elif operation == "DELETE":
+                    if len(sp) < 2:
+                        print("2 arguments required for delete")
+                    else:
+                        store.delete(sp[1])
                 else:
-                    store.set(sp[1], sp[2])
-            elif operation == "DELETE":
-                if len(sp) < 2:
-                    print("2 arguments required for delete")
-                else:
-                    store.delete(sp[1])
-            else:
-                print(f"Invalid operation: {operation}!")
+                    print(f"Invalid operation: {operation}!")
+            file.close()
+    except:
+        print("No file exists!")
 
 if __name__ == "__main__":
     main()
