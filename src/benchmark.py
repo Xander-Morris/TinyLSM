e8fee383be3b60654e6f5b02afa0b60931576fee
Xander <87785817+XanderYZZ@users.noreply.github.com>
2026-03-18 15:32:01 -0700

    Finished benchmarks for now.

--- src/benchmark.py
+++ src/benchmark.py
@@ -7,14 +7,15 @@ import src.config as config
 
 def do_benchmark_funct(store, n, funct_type):
     start = time.perf_counter()
+    ops = {
+        "writes": lambda i: store.set(f"test_key_{i}", f"test_value_{i}"),
+        "reads": lambda i: store.get(f"test_key_{i}"),
+        "misses": lambda i: store.get(f"missing_key_{i}")
+    }
+    op = ops[funct_type]
 
     for i in range(0, n):
-        if funct_type == "writes":
-            store.set(f"test_key_{i}", f"test_value_{i}")
-        elif funct_type == "reads":
-            store.get(f"test_key_{i}")
-        elif funct_type == "misses":
-            store.get(f"missing_key_{i}")
+        op(i)
 
     end = time.perf_counter()
     
@@ -39,6 +40,7 @@ def setup():
 def main():
     original_dir = os.getcwd()
     store, pth = setup() 
+    print(f"Doing the benchmarks with N={config.BENCHMARK_N}...")
     total_write_time = benchmark_writes(store, config.BENCHMARK_N)
     print(f"Writes: {config.BENCHMARK_N} ops in {total_write_time:.2f}s -> {int(config.BENCHMARK_N / total_write_time)} ops/sec")
     total_read_time = benchmark_reads(store, config.BENCHMARK_N)
