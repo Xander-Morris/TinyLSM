50cf8849c0b54911180e32c71e869e70f7653889
Xander <87785817+XanderYZZ@users.noreply.github.com>
2026-03-18 16:05:18 -0700

    Fixed utils.py and README.

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,4 @@
-def process_line(store, line, replay=False):
+def process_line(store, line):
     line = line.strip() 
     sp = line.split(" ")
     operation = sp[0]
@@ -7,26 +7,19 @@ def process_line(store, line, replay=False):
         if len(sp) < 3:
             print("3 arguments are required for SET!")
         else:
-            if replay:
-                store._set(sp[1], sp[2], True)
-            else:
-                store.set(sp[1], sp[2])
+            store.set(sp[1], sp[2])
     elif operation == "SCAN":
         if len(sp) < 3:
             print("3 arguments are required for SCAN!")
         else:
             tuples = store.scan(sp[1], sp[2])
-
             for key, value in tuples: 
                 print(f"Key: {key}, value: {value}")
     elif operation == "DELETE":
         if len(sp) < 2:
             print("2 arguments are required for DELETE!")
         else:
-            if replay:
-                store._delete(sp[1], True)
-            else:
-                store.delete(sp[1])
+            store.delete(sp[1])
     elif operation == "GET":
         if len(sp) < 2:
             print("2 arguments are required for GET!")
