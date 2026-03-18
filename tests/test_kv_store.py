74c11dfb369b701f1596a57d157f2ffabb58b020
Xander <87785817+XanderYZZ@users.noreply.github.com>
2026-03-18 14:06:25 -0700

    Added first test, and fixed issues.

--- tests/test_kv_store.py
+++ tests/test_kv_store.py
@@ -8,3 +8,11 @@ def test_set_get(tmp_path):
     store = src.classes.kv_store.KVStore()
     store.set("foo", "bar")
     assert store.get("foo") == "bar"
+
+def test_set_delete_get(tmp_path):
+    os.chdir(tmp_path)
+    store = src.classes.kv_store.KVStore()
+    store.set("foo", "bar")
+    store.delete("foo")
+    assert store.get("foo") == None 
+
