c734726fcb1a9040699b4bda7729a5b06e0895fe
Xander <87785817+XanderYZZ@users.noreply.github.com>
2026-03-18 15:24:46 -0700

    Starting benchmarks.

--- src/benchmark.py
+++ src/benchmark.py
@@ -3,19 +3,31 @@ import os
 import time 
 import tempfile 
 import shutil 
+import src.config as config 
 
-def benchmark_reads(store, n):
-
-def benchmark_misses(store, n):
-
-
-def benchmark_writes(store, n):
+def do_benchmark_funct(store, n, funct_type):
     start = time.perf_counter()
+
     for i in range(0, n):
-        store.set(f"test_key_{i}", f"test_value_{i}")
+        if funct_type == "writes":
+            store.set(f"test_key_{i}", f"test_value_{i}")
+        elif funct_type == "reads":
+            store.get(f"test_key_{i}")
+        elif funct_type == "misses":
+            store.get(f"missing_key_{i}")
+
     end = time.perf_counter()
     
     return end - start 
+
+def benchmark_reads(store, n):
+    return do_benchmark_funct(store, n, "reads")
+
+def benchmark_misses(store, n):
+    return do_benchmark_funct(store, n, "misses")
+
+def benchmark_writes(store, n):
+    return do_benchmark_funct(store, n, "writes")
 
 def setup():
     pth = tempfile.mkdtemp()
@@ -26,7 +38,9 @@ def setup():
 
 def main():
     store = setup() 
-    print(benchmark_writes(store, 10000))
+    print(benchmark_writes(store, config.BENCHMARK_N))
+    print(benchmark_reads(store, config.BENCHMARK_N))
+    print(benchmark_misses(store, config.BENCHMARK_N))
 
 if __name__ == "__main__": 
     main() 
