820892ae9a8a6f869612b8ab71f978378e5702a4
Xander <87785817+XanderYZZ@users.noreply.github.com>
2026-03-18 14:23:56 -0700

    Added test for sstable

--- tests/test_kv_store.py
+++ tests/test_kv_store.py
@@ -3,6 +3,18 @@ import src.classes.kv_store
 import src.config as config 
 import os 
 
+def force_flush(store):
+    for i in range(config.MAX_ENTRIES):
+        store.set(f"foo_{i}", "bar_test")
+
+def force_compaction(store):
+    for i in range(config.MAX_ENTRIES * 10):
+        store.set(f"foo_{i}", "bar_test")
+
+def do_setting(store, setting):
+    for key, value in setting.items():
+        store.set(key, value)
+
 def test_set_get(tmp_path):
     os.chdir(tmp_path)
     store = src.classes.kv_store.KVStore()
@@ -20,14 +32,9 @@ def test_tombstone_after_flush(tmp_path):
     os.chdir(tmp_path)
     store = src.classes.kv_store.KVStore()
     store.set("foo", "bar")
-
-    def force_flush():
-        for i in range(config.MAX_ENTRIES):
-            store.set(f"foo_{i}", "bar_test")
-    
-    force_flush()
+    force_flush(store)
     store.delete("foo")
-    force_flush()
+    force_flush(store)
     store = src.classes.kv_store.KVStore() 
     assert store.get("foo") == None 
 
@@ -35,11 +42,22 @@ def test_wal_replay(tmp_path):
     os.chdir(tmp_path)
     store = src.classes.kv_store.KVStore()
     setting = {"foo": "bar", "xander": "sadie"}
-
-    for key, value in setting.items():
-        store.set(key, value)
+    do_setting(store, setting)
 
     store = src.classes.kv_store.KVStore()
 
     for key, value in setting.items():
         assert store.get(key) == value 
+
+def test_sstable_read_after_flush(tmp_path):
+    os.chdir(tmp_path)
+    store = src.classes.kv_store.KVStore()
+    setting = {"foo": "bar", "xander": "sadie"}
+    do_setting(store, setting)
+    force_flush(store)
+
+    for key, value in setting.items():
+        assert store.get(key) == value
+
+def test_compaction(tmp_path):
+    
