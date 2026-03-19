e4f4da8853b6b4cea1b63258f06adee5734ab8f6
Xander <87785817+XanderYZZ@users.noreply.github.com>
2026-03-18 18:31:50 -0700

    Starting multithreading approach for reading.

--- src/classes/read_write_lock.py
+++ src/classes/read_write_lock.py
@@ -10,15 +10,23 @@ class ReadWriteLock:
         with self._condition: 
             while self._writing:
                 self._condition.wait()
-        
             self._readers += 1 
 
     def release_read(self):
         with self._condition: 
             self._readers -= 1
-
             if self._readers > 0:
                 return
-
             # Notify waiting writers that the _readers count is 0.
+            self._condition.notify_all()
+
+    def acquire_write(self):
+        with self._condition: 
+            while self._writing or self._readers > 0:
+                self._condition.wait()
+            self._writing = True 
+
+    def release_write(self):
+        with self._condition: 
+            self._writing = False 
             self._condition.notify_all()
