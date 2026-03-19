19c8b731f693889d150629281e86ce3c29855e1d
Xander <87785817+XanderYZZ@users.noreply.github.com>
2026-03-19 12:03:53 -0700

    Refactoring tests code.

--- tests/test_durability.py
+++ tests/test_durability.py
@@ -6,5 +6,14 @@ def test_checksum_corruption(store):
     store.set("xander", "test")
     force_flush(store)
     files = [f for f in glob.glob("sst_*") if "." not in f]
-    file = files[0]
-    print(file)
+
+    with open(files[0], 'r') as file: 
+        lines = file.readlines() 
+    
+    lines[0] = lines[0][:5] + "X" + lines[0][6:]
+
+    with open(files[0], 'w') as file: 
+        file.writelines(lines)
+
+    with pytest.raises(ValueError):
+        store.get("xander")
