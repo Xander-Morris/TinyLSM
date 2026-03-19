08fa4d9cd25bb1cacb61722826511f03213257c4
Xander <87785817+XanderYZZ@users.noreply.github.com>
2026-03-19 12:01:58 -0700

    Refactoring tests code.

--- tests/test_durability.py
+++ tests/test_durability.py
@@ -1,6 +1,10 @@
 import pytest 
+import glob 
 from conftest import force_flush, force_compaction, do_setting, assert_all_readable
 
 def test_checksum_corruption(store):
     store.set("xander", "test")
     force_flush(store)
+    files = [f for f in glob.glob("sst_*") if "." not in f]
+    file = files[0]
+    print(file)
