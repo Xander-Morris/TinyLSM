6cb252880bfa86457100f508a271c039c59bf5f9
Xander <87785817+XanderYZZ@users.noreply.github.com>
2026-05-26 12:58:45 -0700

    Fixing correctness bugs.

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,18 +3,16 @@ import src.config as config
 import src.classes.kv_store as kv_store
 import os
 
-# This is to automatically have the "store" as a variable passed to each testing function. 
 @pytest.fixture 
 def store(tmp_path):
-    os.chdir(tmp_path)
-    return kv_store.KVStore()
-
-@pytest.fixture 
-def store(tmp_path):
+    old_cwd = os.getcwd()
     os.chdir(tmp_path)
     s = kv_store.KVStore() 
-    yield s
-    s.close() 
+    try:
+        yield s
+    finally:
+        s.close()
+        os.chdir(old_cwd)
 
 def force_flush(store, make_sure_after_normal_entries=False):
     bytes_written = 0
